53a53357078c1fdb43a7dfff2ded27484d22ae56
onionsalot <trong.nguyen1008@gmail.com>
2021-10-28 13:55:11 -0400

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello' # syntax for controller + method
+  root 'pages#home' # syntax for controller + method
 end
