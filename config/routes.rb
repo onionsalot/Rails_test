08c07de49091137de733dad664c00e2d17679ec7
onionsalot <trong.nguyen1008@gmail.com>
2021-10-31 20:39:32 -0400

    adds scaoffoling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home' # syntax for controller + method
   get 'about', to: 'pages#about'
+    # Steps:
+  # -> Create routes in routes.rb
+  # -> Create controller with "rails generate controller pages"
+  # -> Create html.erb file in the views folder
 end
